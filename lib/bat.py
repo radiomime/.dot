8bb6bf4f63053300f0b776419962296ede0d87b6
Matthew Wright <wright.matthew91@gmail.com>
2021-02-17 11:23:13 -0700

    clean: python black spacing

--- lib/bat.py
+++ lib/bat.py
@@ -29,49 +29,57 @@ class Bat:
     def __init__(self):
         self.os = getSys()
         self.user = getpass.getuser()
-        self.path = '/usr/local/bin'
-
+        self.path = "/usr/local/bin"
 
     def install(self):
-        print('*** installing bat')
-        if self.os == 'linux':
+        print("*** installing bat")
+        if self.os == "linux":
             self.__linuxInstall()
         else:
-            print('no install instructions for', self.os)
+            print("no install instructions for", self.os)
 
     def uninstall(self):
-        if self.os == 'linux':
-            print('no uninstall instructions for', self.os)
+        if self.os == "linux":
+            print("no uninstall instructions for", self.os)
         else:
-            print('no uninstall instructions for', self.os)
+            print("no uninstall instructions for", self.os)
 
     def __linuxInstall(self):
-        batVersion = getLatestGithubRepo('sharkdp/bat')['name']
+        batVersion = getLatestGithubRepo("sharkdp/bat")["name"]
 
-        repo = "".join([
-            'https://github.com/sharkdp/bat/releases/download/',
-            batVersion, '/',
-            'bat_',
-            batVersion[1:],
-            '_amd64.deb'
-        ])
+        repo = "".join(
+            [
+                "https://github.com/sharkdp/bat/releases/download/",
+                batVersion,
+                "/",
+                "bat_",
+                batVersion[1:],
+                "_amd64.deb",
+            ]
+        )
 
-        subprocess.run([
-            'curl',
-            '-fsSL',
-            repo,
-            '-o',
-            "".join('bat_amd64.deb'),
-        ])
+        subprocess.run(
+            [
+                "curl",
+                "-fsSL",
+                repo,
+                "-o",
+                "".join("bat_amd64.deb"),
+            ]
+        )
 
-        subprocess.run([
-            'sudo',
-            'dpkg',
-            '-i',
-            'bat_amd64.deb',
-        ])
+        subprocess.run(
+            [
+                "sudo",
+                "dpkg",
+                "-i",
+                "bat_amd64.deb",
+            ]
+        )
 
-        subprocess.run([
-            'rm',
-            'bat_amd64.deb',
-        ])
+        subprocess.run(
+            [
+                "rm",
+                "bat_amd64.deb",
+            ]
+        )
