4b40a6f154b0e83c17fa000176efc9084f7f3772
Matthew Wright <wright.matthew91@gmail.com>
2021-05-07 00:40:00 -0600

    clean: linting

--- lib/go.py
+++ lib/go.py
@@ -1,56 +1,44 @@
 #!/usr/bin/env python3
-from sys import platform
-import json
-import sys
-from os.path import expanduser
-from os.path import abspath
-from shutil import which
-import time
-import subprocess
-import os
-import requests
 import getpass
+import json
+import os
+import subprocess
+import sys
+import time
+from os.path import abspath, expanduser
+from shutil import which
+from sys import platform
 
-from .util import (
-    getPyInterpreter,
-    getSys,
-    getLatestGithubRepo,
-)
+import requests
 
 from .snap import Snap
+from .util import getLatestGithubRepo, getPyInterpreter, getSys
+
 
 class Go:
     def __init__(self):
-        print('*** performing actions for Go package')
+        print("*** performing actions for Go package")
         self.os = getSys()
 
     def checkInstall(self):
-        return which('go') is not None
+        return which("go") is not None
 
     def install(self):
-        if self.os == 'linux':
+        if self.os == "linux":
             self.__linuxInstall()
         else:
-            print('no install instructions for', self.os)
+            print("no install instructions for", self.os)
 
     def __linuxInstall(self):
         if not self.checkInstall():
-            subprocess.run(['sudo',
-                            'snap',
-                            'install',
-                            'go',
-                            '--classic'
-                            ])
-            subprocess.run(['go', 'version'])
+            subprocess.run(["sudo", "snap", "install", "go", "--classic"])
+            subprocess.run(["go", "version"])
         self.update()
 
     def update(self):
         if self.checkInstall():
-            subprocess.run(['sudo', 'snap', 'refresh', 'go'])
+            subprocess.run(["sudo", "snap", "refresh", "go"])
 
     def uninstall(self):
         if self.checkInstall():
-            subprocess.run(['sudo',
-                            'snap',
-                            'remove',
-                            'go'])
+            subprocess.run(["sudo", "snap", "remove", "go"])
