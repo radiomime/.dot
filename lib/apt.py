396fe23d1b413ad156e44dc9c2dc348002f3cd8f
Matthew Wright <wright.matthew91@gmail.com>
2021-05-07 00:39:40 -0600

    clean: linting

--- lib/apt.py
+++ lib/apt.py
@@ -1,97 +1,89 @@
-from sys import platform
-import json
-import sys
-from os.path import expanduser
-from os.path import abspath
-from shutil import which
-import time
-import subprocess
 import os
-import requests
-import getpass
+import subprocess
 
-from .util import (
-    getPyInterpreter,
-    getSys,
-)
 
 class Apt:
-    def __init__(self):
-        self.os = getSys()
-        self.user = getpass.getuser()
+    # def __init__(self):
+    #     self.user = getpass.getuser()
 
     def update(self):
-        print('*** apt update')
-        subprocess.run([
-            'sudo',
-            'apt-get',
-            '-qq',
-            'update',
-        ])
+        print("*** apt update")
+        subprocess.run(
+            [
+                "sudo",
+                "apt-get",
+                "-qq",
+                "update",
+            ]
+        )
 
     def upgrade(self):
-        print('*** apt upgrade')
-        subprocess.run([
-            'sudo',
-            'apt-get',
-            '-qq',
-            'upgrade',
-        ])
+        print("*** apt upgrade")
+        subprocess.run(
+            [
+                "sudo",
+                "apt-get",
+                "-qq",
+                "upgrade",
+            ]
+        )
 
     def install(self, pkgs):
         if not isinstance(pkgs, list):
-            pkgs = [ pkgs ]
+            pkgs = [pkgs]
 
         cmd = [
-            'sudo',
-            'apt-get',
-            '-qq',
-            'install',
-            '-y',
+            "sudo",
+            "apt-get",
+            "-qq",
+            "install",
+            "-y",
         ]
         cmd.extend(pkgs)
 
-        print('installing:', pkgs)
+        print("installing:", pkgs)
         subprocess.run(cmd)
 
+    def uninstall(self, pkgs):
+        if not isinstance(pkgs, list):
+            pkgs = [pkgs]
 
+        cmd = [
+            "sudo",
+            "apt-get",
+            "-qq",
+            "remove",
+            "-y",
+        ]
+        cmd.extend(pkgs)
 
+        print("removing:", pkgs)
+        subprocess.run(cmd)
 
-    def uninstall(self, pkg):
-        print('uninstall not yet implemented', pkg)
+    def add_gpg_key(self, link):
+        print("adding gpg key from:", link)
+        subprocess.run(["curl", "-fsSL", link, "-o", "gpg-key-to-add"])
 
-    def addGpgKey(self, link):
-        print('adding gpg key from:', link)
-        subprocess.run([
-            'curl',
-            '-fsSL',
-            link,
-            '-o', 'gpg-key-to-add'
-        ])
+        subprocess.run(["sudo", "apt-key", "add", "gpg-key-to-add"])
 
-        subprocess.run([
-            'sudo',
-            'apt-key',
-            'add',
-            'gpg-key-to-add'
-        ])
+        subprocess.run(["rm", "gpg-key-to-add"])
 
-        subprocess.run([
-            'rm',
-            'gpg-key-to-add'
-        ])
+    def gpg_fingerprint(self, last8):
+        subprocess.run(
+            [
+                "sudo",
+                "apt-key",
+                "fingerprint",
+                last8,
+            ]
+        )
 
-    def gpgFingerprint(self, last8):
-        subprocess.run([
-            'sudo',
-            'apt-key',
-            'fingerprint',
-            last8,
-        ])
-
-    def addAptRepo(self, repo):
-        subprocess.run([
-            'sudo',
-            'add-apt-repository',
-            repo,
-        ], stdout=open(os.devnull, 'wb'))
+    def add_apt_repo(self, repo):
+        subprocess.run(
+            [
+                "sudo",
+                "add-apt-repository",
+                repo,
+            ],
+            stdout=open(os.devnull, "wb"),
+        )
