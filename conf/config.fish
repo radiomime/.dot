6cbf1b3eee9ad1774dba9891550c4673602eb929
Matthew Wright <wright.matthew91@gmail.com>
2021-05-11 19:32:16 -0600

    feat: favorite abbreviations

--- conf/config.fish
+++ conf/config.fish
@@ -1,2 +1,15 @@
+if status --is-interactive
+    abbr --add --global mk 'mkdir -pv'
+    abbr --add --global v 'nvim'
+    abbr --add --global b 'bat'
+    abbr --add --global c 'clear'
+    abbr --add --global l 'ls'
+    abbr --add --global ll 'ls -alth'
+    abbr --add --global x 'chmod +x'
+    abbr --add --global ts 'tmux ls'
+    abbr --add --global tn 'tmux new -s'
+    abbr --add --global ta 'tmux a -t'
+end
+
 # Starship at the end of fish config
 starship init fish | source
