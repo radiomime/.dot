1cfa16223a5b9e41e3b036bf69b1bf5b42a0ba97
Matthew Wright <wright.matthew91@gmail.com>
2021-05-11 19:32:34 -0600

    clean: this is an abbreviation

--- conf/fish_functions/c.fish
+++
@@ -1,6 +0,0 @@
-function c
-    # The function is called ls,
-    # so we have to explicitly call `command ls` to avoid calling ourselves.
-    # command ls --color=auto $argv
-    command clear
-end
