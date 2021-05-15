d4c3afe6a3b9c0d4b24b590ec08367acc5adf74c
Matthew Wright <wright.matthew91@gmail.com>
2021-05-15 16:27:42 -0600

    feat: exa

--- conf/config.fish
+++ conf/config.fish
@@ -3,13 +3,19 @@ if status --is-interactive
     abbr --add --global v 'nvim'
     abbr --add --global b 'bat'
     abbr --add --global c 'clear'
-    abbr --add --global l 'ls'
-    abbr --add --global ll 'ls -alth'
+    abbr --add --global l 'exa'
+    abbr --add --global ll 'exa --long --all'
+    abbr --add --global lll 'exa --long --all --tree --level=2'
+    # abbr --add --global l 'ls'
+    # abbr --add --global ll 'ls -alth'
     abbr --add --global x 'chmod +x'
     abbr --add --global ts 'tmux ls'
     abbr --add --global tn 'tmux new -s'
     abbr --add --global ta 'tmux a -t'
 end
+
+# rust path
+set PATH $HOME/.cargo/bin $PATH
 
 # Starship at the end of fish config
 starship init fish | source
---
+++ lib/exa.py
@@ -0,0 +1,38 @@
+import subprocess
+
+from .abs_package import Package
+from .apt import Apt
+from .rust import Rust
+from .util import github_release_metadata, is_installed
+
+
+class Exa(Package):
+    def __init__(self):
+        super().__init__()
+
+    def is_installed(self):
+        return is_installed("exa")
+
+    def get_version(self):
+        output = subprocess.check_output(
+            [
+                "exa",
+                "--version",
+            ]
+        )
+        output = output.decode("utf-8")
+        for line in output.split("\n"):
+            words = line.split(" ")
+            if words[0][0] == "v":
+                return words[0][1:]
+
+        # should never be hit
+        return None
+
+    def linux_install(self):
+        rust = Rust()
+        rust.cargo_install("exa")
+
+    def linux_uninstall(self):
+        apt = Apt()
+        apt.uninstall("exa")
