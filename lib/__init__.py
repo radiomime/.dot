fea974dc3d7c9605dd0aab040a7b53efff9ff0dd
Matthew Wright <wright.matthew91@gmail.com>
2021-05-15 16:29:54 -0600

    feat: exa, bpytop, exa, rust, nerdfonts

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,16 +2,25 @@ __version__ = "0.1.0"
 
 
 from .bat import Bat
+from .bpytop import Bpytop
 from .docker import Docker
 from .dot import Dot
+from .exa import Exa
 from .fish import Fish
 from .kitty import Kitty
 from .mitmproxy import Mitmproxy
 from .neovim import Neovim
+from .nerdfonts import Nerdfonts
 from .ripgrep import Ripgrep
+from .rust import Rust
+from .snap import Snap
 from .starship import Starship
 from .tldr import Tldr
 
 # from .data_source import DataSource
 # from .file_io import FileIO
 # from .report import Report
+# cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
+
+# curl -fLo "Meslo LG M Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/M/Regular/complete/Meslo%20LG%20M%20Regular%20Nerd%20Font%20Complete.ttf
