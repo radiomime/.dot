6377f10197f6c3054eb582a8287bb85007f50dea
Matthew Wright <wright.matthew91@gmail.com>
2021-05-16 22:12:34 -0600

    feat: abs package subclass

--- lib/poetry.py
+++ lib/poetry.py
@@ -1,72 +1,58 @@
 #!/usr/bin/env python3
-from sys import platform
-import json
-import sys
-from shutil import which
-import time
 import subprocess
-import os
 
+from .abs_package import Package
 from .apt import Apt
-from .util import (
-    getPyInterpreter,
-    getSys,
-    getLatestGithubRepo,
-)
+from .util import is_installed
 
-class Poetry:
+
+class Poetry(Package):
     def __init__(self):
-        print('*** performing actions for Poetry package')
+        super().__init__()
 
-        self.pyInterpreter = getPyInterpreter()
-        subprocess.run([
-            'curl',
-            '-sSL',
-            ''.join([
-                'https://raw.githubusercontent.com/',
-                'python-poetry/poetry/master/get-poetry.py',
-            ]),
-            '-o', 'get-poetry.py'])
-        self.os = getSys()
+    def is_installed(self):
+        return is_installed("poetry")
 
-    def checkInstall(self):
-        return which('poetry') is not None
+    def get_version(self):
+        output = subprocess.check_output(
+            [
+                "poetry",
+                "--version",
+            ]
+        )
+        output = output.decode("utf-8")
+        for line in output.split("\n"):
+            words = line.split(" ")
+            if words[0] == "Poetry":
+                return words[2]
 
-    def install(self):
-        if self.os == 'linux':
-            self.__linuxInstall()
-        else:
-            print('no install instructions for', self.os)
+        # should never be hit
+        return None
 
-    def __linuxInstall(self):
-        # apt dependencies
+    def linux_install(self):
         apt = Apt()
-        apt.install('python3-venv')
+        apt.install("python3-venv")
 
-        if not self.checkInstall():
-            subprocess.run([self.pyInterpreter,
-                            'get-poetry.py', '-y'])
-            subprocess.run(['sh',
-                            '.', '~/.poetry/env'])
-            subprocess.run(['poetry', '--version'])
+        self.run_from_curled_python_script(
+            address="".join(
+                [
+                    "https://raw.githubusercontent.com/",
+                    "python-poetry/poetry/master/get-poetry.py",
+                ]
+            ),
+            args=["-y"],
+        )
 
-        self.update()
-
-
-    def update(self):
-        if self.checkInstall():
-            subprocess.run(['poetry', 'self', 'update'])
-
-    def uninstall(self):
-        if self.checkInstall():
-            subprocess.run([
-                self.pyInterpreter,
-                'get-poetry.py',
-                '--uninstall',
-            ])
-
-    def __del__(self):
-        subprocess.run([
-            'rm',
-            'get-poetry.py',
-        ])
+    def linux_uninstall(self):
+        self.run_from_curled_python_script(
+            address="".join(
+                [
+                    "https://raw.githubusercontent.com/",
+                    "python-poetry/poetry/master/get-poetry.py",
+                ]
+            ),
+            args=[
+                "-y",
+                "--uninstall",
+            ],
+        )
