9ae3c7dc59f33278f7f3ddc3c15784b1209f6d25
Matthew Wright <wright.matthew91@gmail.com>
2021-06-01 14:46:00 -0600

    feat: hammerspoon on osx

---
+++ lib/hammerspoon.py
@@ -0,0 +1,47 @@
+import subprocess
+from .brew import Brew
+
+from .abs_package import Package
+from .util import github_release_metadata, is_installed ,is_installed_osx_app
+
+
+class Hammerspoon(Package):
+    def __init__(self):
+        super().__init__()
+
+    def is_installed(self):
+        return is_installed("hammerspoon")
+
+    def get_version(self):
+        if is_installed_osx_app('hammerspoon'):
+            print('info: mac app versions unsupported')
+            return None
+
+        output = subprocess.check_output(
+            [
+                "hammerspoon",
+                "--version",
+            ]
+        )
+        output = output.decode("utf-8")
+        for line in output.split("\n"):
+            words = line.split(" ")
+            if words[0] == "Mitmproxy:":
+                return words[1]
+
+        # should never be hit
+        return None
+
+    def osx_install(self):
+        brew = Brew()
+        brew.brew_install(
+            pkgs="hammerspoon",
+            flags="--cask",
+        )
+
+    def osx_uninstall(self):
+        brew = Brew()
+        brew.brew_uninstall(
+            pkgs="hammerspoon",
+        )
+
