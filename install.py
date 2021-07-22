d4a51672b6b3b49f078bcb908a4a7cfbe02984e6
Matthew Wright <wright.matthew91@gmail.com>
2021-07-22 19:48:43 +0000

    clean: uncommented

--- install.py
+++ install.py
@@ -41,41 +41,41 @@ from lib import (
 def install():
     print("*** installing neovim and dotfiles")
     # Bat.
-    # Brew().install()
-    # Neovim().install()
-    # Dot().install()
+    Brew().install()
+    Neovim().install()
+    Dot().install()
 
-    # # print("*** installing packages")
-    # Mitmproxy().install()
-    # Nerdfonts().install()
-    # Bat().install()
-    # # Pyright().install()
-    # Bandwhich().install()
-    # Tmux().install()
-    # Hammerspoon().install()
-    # Exa().install()
-    # Rust().install()
-    # Ripgrep().install()
+    # print("*** installing packages")
+    Mitmproxy().install()
+    Nerdfonts().install()
+    Bat().install()
+    # Pyright().install()
+    Bandwhich().install()
+    Tmux().install()
+    Hammerspoon().install()
+    Exa().install()
+    Rust().install()
+    Ripgrep().install()
     Kitty().install()
-    # Fish().install()
-    # Tldr().install()
-    # Starship().install()
+    Fish().install()
+    Tldr().install()
+    Starship().install()
 
-    # Snap().install()
-    # Go().install()
-    # Poetry().install()
-    # Terraform().install()
-    # Node().install()
-    # Yarn().install()
-    # Watchexec().install()
-    # # SwiftFormat().install()
+    Snap().install()
+    Go().install()
+    Poetry().install()
+    Terraform().install()
+    Node().install()
+    Yarn().install()
+    Watchexec().install()
+    # SwiftFormat().install()
 
-    # # Docker and k8s. Order matters.
-    # # TODO: minikube should install docker, etc
-    # Docker().install()
+    # Docker and k8s. Order matters.
+    # TODO: minikube should install docker, etc
+    Docker().install()
 
-    # Minikube().install()
-    # Kubectl().install()
+    Minikube().install()
+    Kubectl().install()
 
 
 def uninstall():
