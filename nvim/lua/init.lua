18420e8caa4695ce44461347fa4cd8dbe968bb40
Matthew Wright <wright.matthew91@gmail.com>
2021-08-28 23:36:38 -0600

    feat: init.lua partially changed

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -6,6 +6,9 @@ print('*** Inside init.lua')
 print('home_dir: ' .. home_dir)
 print('os information: ' .. vim.inspect(vim.loop.os_uname()))
 
+-- This is our global config object
+neo = {}
+
 -- CHANGE: Do not use lvim or lunarvim paths
 -- vim.opt.rtp:append(home_dir .. "/.local/share/lunarvim/lvim")
 
@@ -34,17 +37,25 @@ local function file_exists(name)
   end
 end
 
--- local lvim_path = os.getenv "HOME" .. "/.config/lvim/"
--- USER_CONFIG_PATH = lvim_path .. "config.lua"
--- local config_exist = file_exists(USER_CONFIG_PATH)
--- if not config_exist then
---   USER_CONFIG_PATH = lvim_path .. "lv-config.lua"
---   print "Rename ~/.config/lvim/lv-config.lua to config.lua"
--- end
+-- Get reference to config to overwrite default config later
+-- TODO: I can just have one config
+-- NOTE: Anything not specified as local is defined globally
+local nvim_path = os.getenv "HOME" .. "/.config/nvim/"
+USER_CONFIG_PATH = nvim_path .. "config.lua"
+local config_exist = file_exists(USER_CONFIG_PATH)
 
--- require "default-config"
--- local autocmds = require "core.autocmds"
--- require("settings").load_options()
+if not config_exist then
+    print('config does not exist at '.. USER_CONFIG_PATH)
+    USER_CONFIG_PATH = nvim_path .. "lv-config.lua"
+end
+
+-- Import default config
+require "default-config"
+
+
+
+local autocmds = require "core.autocmds"
+require("settings").load_options()
 
 -- local status_ok, error = pcall(vim.cmd, "luafile " .. USER_CONFIG_PATH)
 -- if not status_ok then
@@ -52,13 +63,13 @@ end
 --   print(error)
 -- end
 -- require("settings").load_commands()
--- autocmds.define_augroups(lvim.autocommands)
+-- autocmds.define_augroups(nvim.autocommands)
 
 -- local plugins = require "plugins"
 -- local plugin_loader = require("plugin-loader").init()
--- plugin_loader:load { plugins, lvim.plugins }
--- vim.g.colors_name = lvim.colorscheme -- Colorscheme must get called after plugins are loaded or it will break new installs.
--- vim.cmd("colorscheme " .. lvim.colorscheme)
+-- plugin_loader:load { plugins, nvim.plugins }
+-- vim.g.colors_name = nvim.colorscheme -- Colorscheme must get called after plugins are loaded or it will break new installs.
+-- vim.cmd("colorscheme " .. nvim.colorscheme)
 
 -- local utils = require "utils"
 -- utils.toggle_autoformat()
@@ -70,13 +81,13 @@ end
 -- local null_status_ok, null_ls = pcall(require, "null-ls")
 -- if null_status_ok then
 --   null_ls.config {}
---   require("lspconfig")["null-ls"].setup(lvim.lsp.null_ls.setup)
+--   require("lspconfig")["null-ls"].setup(nvim.lsp.null_ls.setup)
 -- end
 
 -- local lsp_settings_status_ok, lsp_settings = pcall(require, "nlspsettings")
 -- if lsp_settings_status_ok then
 --   lsp_settings.setup {
---     config_home = os.getenv "HOME" .. "/.config/lvim/lsp-settings",
+--     config_home = os.getenv "HOME" .. "/.config/nvim/lsp-settings",
 --   }
 -- end
 
@@ -85,8 +96,8 @@ end
 require('tester')
 
 -- -- TODO: these guys need to be in language files
--- -- if lvim.lang.emmet.active then
+-- -- if nvim.lang.emmet.active then
 -- --   require "lsp.emmet-ls"
 -- -- end
--- -- if lvim.lang.tailwindcss.active then
+-- -- if nvim.lang.tailwindcss.active then
 -- --   require "lsp.tailwind
