1bdd6c36e2d2f7360aee0f4dcb5718855e7bcbd2
Matthew Wright <wright.matthew91@gmail.com>
2021-09-06 02:00:50 -0600

    clean: defaults

--- nvim/lua/core/terminal.lua
+++ nvim/lua/core/terminal.lua
@@ -6,8 +6,7 @@ M.config = function()
         on_config_done = nil,
         -- size can be a number or function which is passed the current terminal
         size = 20,
-        -- open_mapping = [[<c-\>]],
-        open_mapping = [[<c-t>]],
+        open_mapping = [[<c-t>]], -- ctrl-t opens and closes the terminal
         hide_numbers = true, -- hide the number column in toggleterm buffers
         shade_filetypes = {},
         shade_terminals = true,
@@ -15,8 +14,7 @@ M.config = function()
         start_in_insert = true,
         insert_mappings = true, -- whether or not the open mapping applies in insert mode
         persist_size = false,
-        -- direction = 'vertical' | 'horizontal' | 'window' | 'float',
-        direction = "float",
+        direction = "float", -- 'vertical' | 'horizontal' | 'window' | 'float'
         close_on_exit = true, -- close the terminal window when the process exits
         shell = vim.o.shell, -- change the default shell
         -- This field is only relevant if direction is set to 'float'
