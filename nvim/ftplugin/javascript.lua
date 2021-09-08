6b654020079505220286037d1ae4f57b83d667a9
Matthew Wright <wright.matthew91@gmail.com>
2021-09-07 21:31:42 -0600

    feat: javascript ft plugin

--- nvim/ftplugin/javascript.lua
+++ nvim/ftplugin/javascript.lua
@@ -1,1 +1,9 @@
-require("lsp").setup "javascript"
+require("lsp").setup("javascript")
+print(
+    "hello, from javascript startup! remember to run: :TSInstall javascript, and :LspInstall typescript"
+)
+-- TODO: make this automatic!!
+-- print("remember to run")
+-- print(":TSInstall javascript")
+-- Remember to run:
+--     :TSInstall javascript
