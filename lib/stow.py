2035ec6ab92c3ad3d6ca67194736a7f0a29a2b65
Matthew Wright <wright.matthew91@gmail.com>
2021-10-18 01:34:20 -0600

    feat: stow

---
+++ lib/stow.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import subprocess
+from os.path import abspath, expanduser
+from .apt import Apt
+from .brew import Brew
+
+from .abs_package import Package
+
+from .util import github_release_metadata, is_installed
+
+
+class Stow(Package):
+    def __init__(self, config_dir=abspath(expanduser("~/.config/nvim"))):
+        super().__init__()
+
+    def is_installed(self):
+        return is_installed("stow")
+
+    def get_version(self):
+        output = subprocess.check_output(
+            [
+                "stow",
+                "--version",
+            ]
+        )
+        output = output.decode("utf-8")
+
+        words = output.split(" ")
+        if words[0] == "stow":
+            return words[4].strip()
+
+        # should never be hit
+        return None
+
+    def linux_install(self):
+        apt = Apt()
+        apt.install(["stow"])
+
+    def linux_uninstall(self):
+        apt = Apt()
+        apt.uninstall(["stow"])
+
+
+#     def osx_install(self):
+#         self.__install()
+#
+#     def osx_uninstall(self):
+#         self.__uninstall()
