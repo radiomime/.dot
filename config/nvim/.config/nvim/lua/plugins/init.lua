24f38de63c7a1cd037bc8e0e6df8f64dd765ba26
Matthew Wright <wright.matthew91@gmail.com>
2021-11-21 00:20:59 -0700

    feat: more plugins!

--- config/nvim/.config/nvim/lua/plugins/init.lua
+++ config/nvim/.config/nvim/lua/plugins/init.lua
@@ -69,36 +69,32 @@ function plugins:install()
 
         -- lsp installer
         use("williamboman/nvim-lsp-installer")
-        --   use insteaad of
-        -- {
-        --     "kabouzeid/nvim-lspinstall",
-        --     event = "VimEnter",
-        --     config = function()
-        --         local lspinstall = require("core.lspinstall")
-        --         lspinstall.setup()
-        --     end,
-        -- },
 
         -- telescope
-        use({
-            "nvim-telescope/telescope.nvim",
-            requires = {{ "nvim-lua/plenary.nvim" }},
-            config = require('plugins.telescope').config
-        })
         -- TODO: look at this as primeagen said
         --   https://github.com/nvim-telescope/telescope-fzf-native.nvim
+         use({
+             "nvim-telescope/telescope.nvim",
+             requires = {{ "nvim-lua/plenary.nvim" }},
+             config = require('plugins.telescope').config
+         })
+         use({
+             "nvim-telescope/telescope-fzf-native.nvim",
+             run = "make",
+             disable = false,
+         })
 
         -- completion and snippets
         use({
           "hrsh7th/nvim-cmp",
-          requires = {
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-nvim-lua",
-          },
+           requires = {
+             "L3MON4D3/LuaSnip",
+             "saadparwaiz1/cmp_luasnip",
+             "hrsh7th/cmp-buffer",
+             "hrsh7th/cmp-nvim-lsp",
+             "hrsh7th/cmp-path",
+             "hrsh7th/cmp-nvim-lua",
+           },
           config = require('plugins.nvim-cmp').config,
           run = function()
             -- cmp's config requires cmp to be installed to run the first time
@@ -106,7 +102,6 @@ function plugins:install()
                 require('plugins.nvim-cmp').config()
             end
           end,
-
         })
 
         -- which key
@@ -116,13 +111,31 @@ function plugins:install()
           config = require('plugins.which-key').config,
           -- event =  "BufWinEnter",
           disable = false,
-          -- run = function()
-          --   -- cmp's config requires cmp to be installed to run the first time
-          --   if not plugins.nvim-cmp then
-          --       require('plugins.nvim-cmp').config()
-          --   end
-          -- end,
+        })
 
+        -- treesitter
+        use({
+          "nvim-treesitter/nvim-treesitter",
+          branch = "0.5-compat",
+          -- run = ":TSUpdate",
+          config = require('plugins.nvim-treesitter').config,
+        })
+        
+         -- autopairs
+         use({
+           "windwp/nvim-autopairs",
+           config = require('plugins.nvim-autopairs').config,
+         })
+
+        -- commenting
+        use("tpope/vim-commentary")
+        use("JoosepAlviste/nvim-ts-context-commentstring")
+
+        -- nvim tree
+        use({
+            "kyazdani42/nvim-tree.lua",
+            requires = 'kyazdani42/nvim-web-devicons',
+            config = require('plugins.nvim-tree').config,
         })
 
         -- utility or required by other ones
@@ -132,269 +145,15 @@ function plugins:install()
         -- experimenting
 
         -- tmp fixes
-        -- use "antoinemadec/FixCursorHold.nvim"
+        use("antoinemadec/FixCursorHold.nvim") -- Needed while issue https://github.com/neovim/neovim/issues/12587 is still open
 
-        -- PACKER SUGGESTED BELOW
-        -- PACKER SUGGESTED BELOW
-        -- PACKER SUGGESTED BELOW
-
-        --       -- Simple plugins can be specified as strings
-        --       use '9mm/vim-closer'
-        --
-        --       -- Lazy loading:
-        --       -- Load on specific commands
-        --       use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
-        --
-        --       -- Load on an autocommand event
-        --       use {'andymass/vim-matchup', event = 'VimEnter'}
-        --
-        --       -- Load on a combination of conditions: specific filetypes or commands
-        --       -- Also run code after load (see the "config" key)
-        --       use {
-        --         'w0rp/ale',
-        --         ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
-        --         cmd = 'ALEEnable',
-        --         config = 'vim.cmd[[ALEEnable]]'
-        --       }
-        --
-        --       -- Plugins can have dependencies on other plugins
-        --       use {
-        --         'haorenW1025/completion-nvim',
-        --         opt = true,
-        --         requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
-        --       }
-        --
-        --       -- Plugins can also depend on rocks from luarocks.org:
-        --       use {
-        --         'my/supercoolplugin',
-        --         rocks = {'lpeg', {'lua-cjson', version = '2.1.0'}}
-        --       }
-        --
-        --       -- You can specify rocks in isolation
-        --       use_rocks 'penlight'
-        --       use_rocks {'lua-resty-http', 'lpeg'}
-        --
-        --       -- Local plugins can be included
-        --       use '~/projects/personal/hover.nvim'
-        --
-        --       -- Plugins can have post-install/update hooks
-        --       use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
-        --
-        --       -- Post-install/update hook with neovim command
-        --       use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-        --
-        --       -- Post-install/update hook with call of vimscript function with argument
-        --       use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
-        --
-        --       -- Use specific branch, dependency and run lua file after load
-        --       use {
-        --         'glepnir/galaxyline.nvim', branch = 'main', config = function() require'statusline' end,
-        --         requires = {'kyazdani42/nvim-web-devicons'}
-        --       }
-        --
-        --       -- Use dependency and run lua function after load
-        --       use {
-        --         'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-        --         config = function() require('gitsigns').setup() end
-        --       }
-        --
-        --       -- You can specify multiple plugins in a single call
-        --       use {'tjdevries/colorbuddy.vim', {'nvim-treesitter/nvim-treesitter', opt = true}}
-        --
-        --       -- You can alias plugin names
-        --       use {'dracula/vim', as = 'dracula'}
-
-        -- CURRENT LUNARVIM BELOW
-        -- CURRENT LUNARVIM BELOW
-        -- CURRENT LUNARVIM BELOW
-        --   { "wbthomason/packer.nvim" },
-        --   { "neovim/nvim-lspconfig" },
-        --   { "tamago324/nlsp-settings.nvim" },
-        --   { "jose-elias-alvarez/null-ls.nvim" },
-        --   { "antoinemadec/FixCursorHold.nvim" }, -- Needed while issue https://github.com/neovim/neovim/issues/12587 is still open
-        --   {
-        --     "williamboman/nvim-lsp-installer",
-        --   },
-        --   {
-        --     "rcarriga/nvim-notify",
-        --     disable = not lvim.builtin.notify.active,
-        --   },
-        --   { "Tastyep/structlog.nvim" },
-        --
-        --   { "nvim-lua/popup.nvim" },
-        --   { "nvim-lua/plenary.nvim" },
-        --   -- Telescope
-        --   {
-        --     "nvim-telescope/telescope.nvim",
-        --     config = function()
-        --       require("lvim.core.telescope").setup()
-        --     end,
-        --     disable = not lvim.builtin.telescope.active,
-        --   },
-        --   {
-        --     "nvim-telescope/telescope-fzf-native.nvim",
-        --     run = "make",
-        --     disable = not lvim.builtin.telescope.active,
-        --   },
-        --   -- Install nvim-cmp, and buffer source as a dependency
-        --   {
-        --     "hrsh7th/nvim-cmp",
-        --     config = function()
-        --       require("lvim.core.cmp").setup()
-        --     end,
-        --     requires = {
-        --       "L3MON4D3/LuaSnip",
-        --       "saadparwaiz1/cmp_luasnip",
-        --       "hrsh7th/cmp-buffer",
-        --       "hrsh7th/cmp-nvim-lsp",
-        --       "hrsh7th/cmp-path",
-        --       "hrsh7th/cmp-nvim-lua",
-        --     },
-        --     run = function()
-        --       -- cmp's config requires cmp to be installed to run the first time
-        --       if not lvim.builtin.cmp then
-        --         require("lvim.core.cmp").config()
-        --       end
-        --     end,
-        --   },
-        --   {
-        --     "rafamadriz/friendly-snippets",
-        --     -- event = "InsertCharPre",
-        --     -- disable = not lvim.builtin.compe.active,
-        --   },
-        --
-        --   -- Autopairs
-        --   {
-        --     "windwp/nvim-autopairs",
-        --     -- event = "InsertEnter",
-        --     config = function()
-        --       require("lvim.core.autopairs").setup()
-        --     end,
-        --     disable = not lvim.builtin.autopairs.active,
-        --   },
-        --
-        --   -- Treesitter
-        --   {
-        --     "nvim-treesitter/nvim-treesitter",
-        --     branch = "0.5-compat",
-        --     -- run = ":TSUpdate",
-        --     config = function()
-        --       require("lvim.core.treesitter").setup()
-        --     end,
-        --   },
-        --
-        --   -- NvimTree
-        --   {
-        --     "kyazdani42/nvim-tree.lua",
-        --     -- event = "BufWinOpen",
-        --     -- cmd = "NvimTreeToggle",
-        --     -- commit = "fd7f60e242205ea9efc9649101c81a07d5f458bb",
-        --     config = function()
-        --       require("lvim.core.nvimtree").setup()
-        --     end,
-        --     disable = not lvim.builtin.nvimtree.active,
-        --   },
-        --
-        --   {
-        --     "lewis6991/gitsigns.nvim",
-        --
-        --     config = function()
-        --       require("lvim.core.gitsigns").setup()
-        --     end,
-        --     event = "BufRead",
-        --     disable = not lvim.builtin.gitsigns.active,
-        --   },
-        --
-        --   -- Whichkey
-        --   {
-        --     "folke/which-key.nvim",
-        --     config = function()
-        --       require("lvim.core.which-key").setup()
-        --     end,
-        --     event = "BufWinEnter",
-        --     disable = not lvim.builtin.which_key.active,
-        --   },
-        --
-        --   -- Comments
-        --   {
-        --     "numToStr/Comment.nvim",
-        --     event = "BufRead",
-        --     config = function()
-        --       require("lvim.core.comment").setup()
-        --     end,
-        --     disable = not lvim.builtin.comment.active,
-        --   },
-        --
-        --   -- project.nvim
-        --   {
-        --     "ahmedkhalf/project.nvim",
-        --     config = function()
-        --       require("lvim.core.project").setup()
-        --     end,
-        --     disable = not lvim.builtin.project.active,
-        --   },
-        --
-        --   -- Icons
-        --   { "kyazdani42/nvim-web-devicons" },
-        --
-        --   -- Status Line and Bufferline
-        --   {
-        --     -- "hoob3rt/lualine.nvim",
-        --     "nvim-lualine/lualine.nvim",
-        --     -- "Lunarvim/lualine.nvim",
-        --     config = function()
-        --       require("lvim.core.lualine").setup()
-        --     end,
-        --     disable = not lvim.builtin.lualine.active,
-        --   },
-        --
-        --   {
-        --     "romgrk/barbar.nvim",
-        --     config = function()
-        --       require("lvim.core.bufferline").setup()
-        --     end,
-        --     event = "BufWinEnter",
-        --     disable = not lvim.builtin.bufferline.active,
-        --   },
-        --
-        --   -- Debugging
-        --   {
-        --     "mfussenegger/nvim-dap",
-        --     -- event = "BufWinEnter",
-        --     config = function()
-        --       require("lvim.core.dap").setup()
-        --     end,
-        --     disable = not lvim.builtin.dap.active,
-        --   },
-        --
-        --   -- Debugger management
-        --   {
-        --     "Pocco81/DAPInstall.nvim",
-        --     -- event = "BufWinEnter",
-        --     -- event = "BufRead",
-        --     disable = not lvim.builtin.dap.active,
-        --   },
-        --
-        --   -- Dashboard
-        --   {
-        --     "ChristianChiarulli/dashboard-nvim",
-        --     event = "BufWinEnter",
-        --     config = function()
-        --       require("lvim.core.dashboard").setup()
-        --     end,
-        --     disable = not lvim.builtin.dashboard.active,
-        --   },
-        --
-        --   -- Terminal
-        --   {
-        --     "akinsho/toggleterm.nvim",
-        --     event = "BufWinEnter",
-        --     config = function()
-        --       require("lvim.core.terminal").setup()
-        --     end,
-        --     disable = not lvim.builtin.terminal.active,
-        --   },
-    end)
+        -- sort through
+        use('hrsh7th/vim-vsnip') -- event: InsertEnter
+        use('rafamadriz/friendly-snippets') -- event: InsertCharPre (maybe for cmp?)
+        use({"rcarriga/nvim-notify", disable = false})
+        use({"Tastyep/structlog.nvim", disable = false})
+    end
+    )
 end
 
 function plugins:sync()
