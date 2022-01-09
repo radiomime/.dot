9aa47fdfd59482ce374dd37af531bbf43adc77cb
Matthew Wright <wright.matthew91@gmail.com>
2022-01-09 16:40:44 -0700

    clean: formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,12 +2,8 @@ require("options")
 require("keymaps")
 require("plugins").install()
 require("colorscheme")
-
--- configure cmp before lsp. TODO: is this necessary?
-require("plugins.cmp")
-
+require("plugins.cmp") -- TODO: do I need to configure cmp before lsp?
 require("lsp")
-
 require("plugins").configure()
 
 -- from scratch below
