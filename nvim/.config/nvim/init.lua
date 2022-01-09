f4e32679d2c2c8cbb44d87ff5dc85b63284c0c43
Matthew Wright <wright.matthew91@gmail.com>
2022-01-09 15:08:35 -0700

    clean: format init

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,30 +1,26 @@
-require "options"
--- require "user.options"
+require("options")
+require("keymaps")
+require("plugins")
 require("colorscheme")
 
-require "keymaps"
--- require "user.keymaps"
-
-require "plugins"
--- require "user.plugins"
-
-require "user.colorscheme"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
-require "user.toggleterm"
+-- from scratch below
+require("user.cmp")
+require("user.lsp")
+require("user.telescope")
+require("user.treesitter")
+require("user.autopairs")
+require("user.comment")
+require("user.gitsigns")
+require("user.nvim-tree")
+require("user.bufferline")
+require("user.lualine")
+require("user.toggleterm")
 -- require "user.project"
-require "user.impatient"
-require "user.indentline"
-require "user.alpha"
+require("user.impatient")
+require("user.indentline")
+require("user.alpha")
 
-require "plugins.whichkey"
+require("plugins.whichkey")
 -- require "user.whichkey"
 
-require "user.autocommands"
+require("user.autocommands")
