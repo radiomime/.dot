74acbc659cf2fdfde7daf2785c91335d917e3377
Matthew Wright <wright.matthew91@gmail.com>
2022-01-09 20:52:28 -0700

    feat: format on save

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/lsp/null-ls.lua
@@ -14,6 +14,16 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+		-- diagnostics.flake8
 	},
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+		end
+	end,
 })
