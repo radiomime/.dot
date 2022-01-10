39fc5267b3a8ccae1d788fd84d7e455afec8ec59
Matthew Wright <wright.matthew91@gmail.com>
2022-01-09 20:54:15 -0700

    clean: remove old nvim config references

--- nvim/.config/oldvim/ftplugin/javascript.lua
+++
@@ -1,9 +0,0 @@
-require("lsp").setup("javascript")
-print(
-    "hello, from javascript startup! remember to run: :TSInstall javascript, and :LspInstall typescript"
-)
--- TODO: make this automatic!!
--- print("remember to run")
--- print(":TSInstall javascript")
--- Remember to run:
---     :TSInstall javascript
