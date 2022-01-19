8ae104c762a920f90b8ccbec5c1518597918a5e3
Matthew Wright <wright.matthew91@gmail.com>
2022-01-19 00:24:28 -0700

    feat: install to packages dir

---
+++ packages/install.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+import argparse
+
+from package_factory import PackageFactory
+
+# from lib.packages import (
+#     Bandwhich,
+#     Bat,
+#     Brew,
+#     Docker,
+#     Dot,
+#     Exa,
+#     Fish,
+#     Go,
+#     Hammerspoon,
+#     Kitty,
+#     Kubectl,
+#     Minikube,
+#     Mitmproxy,
+#     Neovim,
+#     Nerdfonts,
+#     Node,
+#     Poetry,
+#     Stow,
+#     Pyright,
+#     Ripgrep,
+#     Rust,
+#     Snap,
+#     Starship,
+#     Terraform,
+#     Tldr,
+#     Tmux,
+#     Watchexec,
+#     Yarn,
+# )
+#
+#
+# def install():
+#     print("*** installing packages ***")
+#     Brew().install()
+#     Neovim().install()
+#     Stow().install()
+#     Dot().install()
+#
+#     Kitty().install()
+#     Starship().install()
+#     Nerdfonts().install()
+#     Fish().install()
+#
+#     Bat().install()
+#     Tmux().install()
+#     Node().install()
+#
+#     Mitmproxy().install()
+#     Pyright().install()
+#     Bandwhich().install()
+#     Hammerspoon().install()
+#
+#     Rust().install()
+#     Docker().install()
+#
+#     Exa().install()
+#     Ripgrep().install()
+#     Tldr().install()
+#     Snap().install()
+#     Go().install()
+#     Poetry().install()
+#     Terraform().install()
+#     Yarn().install()
+#     Watchexec().install()
+#     Minikube().install()
+#     Kubectl().install()
+#
+#
+# def uninstall():
+#     print("uninstalling things")
+#     Dot().uninstall()
+#
+#     Mitmproxy().uninstall()
+#     Go().uninstall()
+#     Nerdfonts().uninstall()
+#     Bandwhich().uninstall()
+#     Tmux().uninstall()
+#     Hammerspoon().uninstall()
+#     Bat().uninstall()
+#     # Pyright().uninstall()
+#     Exa().uninstall()
+#     Rust().uninstall()
+#     Ripgrep().uninstall()
+#     Kitty().uninstall()
+#     Fish().uninstall()
+#     Tldr().uninstall()
+#     Starship().uninstall()
+#     Snap().uninstall()
+#     Poetry().uninstall()
+#     Terraform().uninstall()
+#     Node().uninstall()
+#     Yarn().uninstall()
+#     Minikube().uninstall()
+#     Kubectl().uninstall()
+#     Watchexec().uninstall()
+#
+#     # Below are weird to un/reinstall. Good opportunity for an upgrade
+#     # TODO: explicit install/uninstall
+#     Neovim().uninstall()
+#     Docker().uninstall()
+#     Brew().uninstall()
+
+
+def process_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    # parser.add_argument(
+    #     "-i",
+    #     "--install",
+    #     help="install a package or list of packages",
+    #     nargs="+",
+    #     required=False,
+    # )
+
+    parser.add_argument(
+        "-v", "--verbose", help="increase output verbosity", required=False
+    )
+
+    parser.add_argument(
+        "-u",
+        "--uninstall",
+        help="uninstall dotfiles",
+        action="store_true",
+        required=False,
+    )
+
+    # parser.add_argument(
+    #     "-d",
+    #     "--dry",
+    #     help="dry run: useful for seeing checking base errors",
+    #     action="store_true",
+    #     required=False,
+    # )
+
+    parser.add_argument(
+        "-l",
+        "--list",
+        help="list available packages",
+        # required=False
+        action="store_true"
+        # nargs="+",
+        # default=None,
+    )
+
+    parser.add_argument(
+        "-a",
+        "--add",
+        help="add a package or app if supported",
+        nargs="+",
+        default=None,
+    )
+
+    parser.add_argument(
+        "-r",
+        "--remove",
+        help="remove a package or app if supported",
+        nargs="+",
+        default=None,
+    )
+    return parser.parse_args()
+
+
+def main():
+    print("WARN: Install scripts will run some install commands as sudo")
+    args = process_args()
+    pkgFactory = PackageFactory()
+
+    if args.add:
+        print(args.add)
+    elif args.remove:
+        print(args.remove)
+    elif args.list:
+        print(pkgFactory.get_packages_list())
+    # elif args.uninstall:
+    #     uninstall()
+    # elif args.dry:
+    #     print("exiting without running anything")
+    #     exit()
+    else:
+        # install()
+        print("should install, not though")
+
+
+if __name__ == "__main__":
+    main()
