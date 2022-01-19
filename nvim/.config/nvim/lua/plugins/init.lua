76653571ebdf6d663307407bca9f426a6aa9eab3
Matthew Wright <wright.matthew91@gmail.com>
2022-01-18 22:23:56 -0700

    feat: nlsp-setting contains requires

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -133,7 +133,10 @@ function M:_install()
 		-- LSP
 		use("neovim/nvim-lspconfig") -- enable LSP
 		use("williamboman/nvim-lsp-installer") -- simple to use language server installer
-		use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
+		use({
+			"tamago324/nlsp-settings.nvim", -- language server settings defined in json for
+			requires = { { "neovim/nvim-lspconfig" } },
+		})
 		use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 
 		-- Telescope
