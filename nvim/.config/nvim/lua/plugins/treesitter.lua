ea3715b1fb8f06f01d7e6cb9f7bdf2909c4b64c6
Matthew Wright <wright.matthew91@gmail.com>
2022-01-18 22:21:04 -0700

    feat: add python to disable indent for treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -16,73 +16,73 @@ function M:configure()
 			disable = {}, -- list of language that will be disabled, e.g. {"latex", "c", "rust"}
 			additional_vim_regex_highlighting = true,
 		},
-    -- TODO: keep indent experimental feature? could cause some issues
+		-- TODO: keep indent experimental feature? could cause some issues
 		indent = {
 			enable = true,
-			disable = { "yaml" }, -- {"python", "html", "javascript"},
+			disable = { "python", "yaml" }, -- {"python", "html", "javascript"},
 		},
-    -- other plugin modules
+		-- other plugin modules
 		context_commentstring = {
 			enable = true,
 			enable_autocmd = false,
-      -- config = {
-      --   -- Languages that have a single comment style
-      --   typescript = "// %s",
-      --   css = "/* %s */",
-      --   scss = "/* %s */",
-      --   html = "<!-- %s -->",
-      --   svelte = "<!-- %s -->",
-      --   vue = "<!-- %s -->",
-      --   json = "",
-      -- },
+			-- config = {
+			--   -- Languages that have a single comment style
+			--   typescript = "// %s",
+			--   css = "/* %s */",
+			--   scss = "/* %s */",
+			--   html = "<!-- %s -->",
+			--   svelte = "<!-- %s -->",
+			--   vue = "<!-- %s -->",
+			--   json = "",
+			-- },
 		},
 		autopairs = {
 			enable = true,
 		},
-    -- TODO: use some of these?
-    -- matchup = {
-    --   enable = false, -- mandatory, false will disable the whole extension
-    --   -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
-    -- },
-    -- autotag = { enable = false },
-    -- textobjects = {
-    --   swap = {
-    --     enable = false,
-    --     -- swap_next = textobj_swap_keymaps,
-    --   },
-    --   -- move = textobj_move_keymaps,
-    --   select = {
-    --     enable = false,
-    --     -- keymaps = textobj_sel_keymaps,
-    --   },
-    -- },
-    -- textsubjects = {
-    --   enable = false,
-    --   keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
-    -- },
-    -- playground = {
-    --   enable = false,
-    --   disable = {},
-    --   updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    --   persist_queries = false, -- Whether the query persists across vim sessions
-    --   keybindings = {
-    --     toggle_query_editor = "o",
-    --     toggle_hl_groups = "i",
-    --     toggle_injected_languages = "t",
-    --     toggle_anonymous_nodes = "a",
-    --     toggle_language_display = "I",
-    --     focus_language = "f",
-    --     unfocus_language = "F",
-    --     update = "R",
-    --     goto_node = "<cr>",
-    --     show_help = "?",
-    --   },
-    -- },
-    -- rainbow = {
-    --   enable = false,
-    --   extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-    --   max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-    -- },
+		-- TODO: use some of these?
+		-- matchup = {
+		--   enable = false, -- mandatory, false will disable the whole extension
+		--   -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+		-- },
+		-- autotag = { enable = false },
+		-- textobjects = {
+		--   swap = {
+		--     enable = false,
+		--     -- swap_next = textobj_swap_keymaps,
+		--   },
+		--   -- move = textobj_move_keymaps,
+		--   select = {
+		--     enable = false,
+		--     -- keymaps = textobj_sel_keymaps,
+		--   },
+		-- },
+		-- textsubjects = {
+		--   enable = false,
+		--   keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
+		-- },
+		-- playground = {
+		--   enable = false,
+		--   disable = {},
+		--   updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		--   persist_queries = false, -- Whether the query persists across vim sessions
+		--   keybindings = {
+		--     toggle_query_editor = "o",
+		--     toggle_hl_groups = "i",
+		--     toggle_injected_languages = "t",
+		--     toggle_anonymous_nodes = "a",
+		--     toggle_language_display = "I",
+		--     focus_language = "f",
+		--     unfocus_language = "F",
+		--     update = "R",
+		--     goto_node = "<cr>",
+		--     show_help = "?",
+		--   },
+		-- },
+		-- rainbow = {
+		--   enable = false,
+		--   extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+		--   max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+		-- },
 	})
 end
 
