91376d6ef3bb848bd2649a434b957079697c42f3
Matthew Wright <wright.matthew91@gmail.com>
2022-02-03 23:39:34 -0700

    feat: shell in /etc/shells if it isn't there for installs

--- packages/pkgs/fish.py
+++ packages/pkgs/fish.py
@@ -4,7 +4,7 @@ from typing import Optional
 from .abs_package import AbsPackage
 from .apt import Apt
 from .brew import Brew
-from .util import bin_loc, github_release_metadata, is_installed
+from .util import bin_loc, github_release_metadata, is_installed, get_user_approval
 
 # TODO: be safe when doing chsh!!
 
@@ -32,17 +32,50 @@ class Fish(AbsPackage):
         # should never be hit
         return None
 
-    def __change_shell(self, shell="fish"):
-        print(f"***changing shell to {shell}")
-        shell = bin_loc(shell)
-        if shell:
-            subprocess.run(
-                [
-                    "chsh",
-                    "-s",
-                    shell,
-                ]
+    def __update_shell(self):
+        favorite_shells = ["fish", "bash", "zsh", "sh"]
+
+        for shell in favorite_shells:
+            shell_loc = bin_loc(shell)
+            if shell_loc is not None and get_user_approval(
+                "change shell to ", shell_loc, "?"
+            ):
+                print("updating shell to:", shell_loc)
+                subprocess.run(["chsh", "-s", shell_loc])
+                return
+
+    def __check_for_shell_in_etc_shells(self, shell):
+        shell_loc = bin_loc(shell)
+        file = open("/etc/shells", "r")
+        lines = file.readlines()
+        for line in lines:
+            if shell_loc == line.strip():
+                print("shell is in /etc/shells:", shell_loc)
+                return True
+        print("shell is not in /etc/shells:", shell_loc)
+        return False
+
+    def __add_shell_to_etc_shells(self, shell):
+        shell_loc = bin_loc("fish")
+
+        if shell_loc is not None and not self.__check_for_shell_in_etc_shells(shell):
+            print("adding", shell_loc, "to /etc/shells")
+
+            ps = subprocess.Popen(
+                ("echo", shell_loc),
+                stdout=subprocess.PIPE,
             )
+
+            output = subprocess.check_output(
+                (
+                    "sudo",
+                    "tee",
+                    "-a",
+                    "/etc/shells",
+                ),
+                stdin=ps.stdout,
+            )
+            ps.wait()
 
     def linux_install(self):
         apt = Apt()
@@ -50,59 +83,28 @@ class Fish(AbsPackage):
         apt.update()
         apt.install("fish")
 
-        self.__change_shell()
+        self.__add_shell_to_etc_shells("fish")
+        self.__update_shell()
 
     def linux_uninstall(self):
         apt = Apt()
         apt.uninstall("fish")
 
-        self.__change_shell(shell="bash")
+        self.__update_shell()
 
     def osx_install(self):
-        # brew = Brew()
-        # brew.brew_install(
-        #     pkgs="fish",
-        #     flags="--HEAD",
-        # )
+        brew = Brew()
+        brew.brew_install(
+            pkgs="fish",
+            flags="--HEAD",
+        )
 
-        file = open("/etc/shells", "r")
-        lines = file.readlines()
-        for line in lines:
-            print(line)
-
-        fish_loc = bin_loc("fish")
-        print("fish location here: ", fish_loc)
-        # # subprocess.run([p])
-        # if fish_loc is not None:
-        #     # subprocess.run([
-        #     #     'sudo',
-        #     #     'tee'
-        #     # ])
-        #     ps = subprocess.Popen(
-        #         ("echo", fish_loc),
-        #         stdout=subprocess.PIPE,
-        #     )
-        #     output = subprocess.check_output(
-        #         (
-        #             "sudo",
-        #             "tee",
-        #             "-a",
-        #             "/etc/shells",
-        #         ),
-        #         stdin=ps.stdout,
-        #     )
-        #     ps.wait()
-        #     print("output")
-        #     print(output)
-        #     # with open("/etc/shells", "a") as file:
-        #     #     file.write(fish_loc)
-        #
-        #     self.__change_shell()
+        self.__add_shell_to_etc_shells("fish")
+        self.__update_shell()
 
     def osx_uninstall(self):
         brew = Brew()
         brew.brew_uninstall(
             pkgs="fish",
         )
-
-        # self.__change_shell(shell="zsh")
+        self.__update_shell()
