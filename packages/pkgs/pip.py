a8dc4ca40bc82a20423c9a297e2bf46984a1c7cd
Matthew Wright <wright.matthew91@gmail.com>
2022-02-06 23:56:04 -0700

    clean: old class

--- packages/pkgs/pip.py
+++ packages/pkgs/pip.py
@@ -85,38 +85,3 @@ class Pip(AbsPackage):
 
         print(f"running pip uninstall: {cmd}")
         subprocess.run(cmd)
-
-
-# class Pip:
-#     def install(self, pkgs):
-#         if not isinstance(pkgs, list):
-#             pkgs = [pkgs]
-
-#         cmd = [
-#             "python3",
-#             "-m",
-#             "pip",
-#             "install",
-#             "--user",
-#             "--upgrade",
-#         ]
-#         cmd.extend(pkgs)
-
-#         print("installing:", pkgs)
-#         subprocess.run(cmd)
-
-#     def uninstall(self, pkgs):
-#         if not isinstance(pkgs, list):
-#             pkgs = [pkgs]
-
-#         cmd = [
-#             "python3",
-#             "-m",
-#             "pip",
-#             "uninstall",
-#             "--user",
-#         ]
-#         cmd.extend(pkgs)
-
-#         print("removing:", pkgs)
-#         subprocess.run(cmd)
