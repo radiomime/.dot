e514df36a36bf080778513c4e0db644214f8fa0c
Matthew Wright <wright.matthew91@gmail.com>
2022-02-13 14:02:15 -0700

    feat: prettier default args, respects prettierrc

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  print('error: null_ls is not callable')
-	return
+  print("error: null_ls is not callable")
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -10,22 +10,22 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		-- diagnostics.flake8
-	},
+  debug = false,
+  sources = {
+    formatting.prettier, -- with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
+    -- diagnostics.flake8
+  },
   -- uncomment on_attach fn below for format on save
-	-- on_attach = function(client)
-	-- 	if client.resolved_capabilities.document_formatting then
-	-- 		vim.cmd([[
+  -- on_attach = function(client)
+  -- 	if client.resolved_capabilities.document_formatting then
+  -- 		vim.cmd([[
   --           augroup LspFormatting
   --               autocmd! * <buffer>
   --               autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
   --           augroup END
   --           ]])
-	-- 	end
-	-- end,
+  -- 	end
+  -- end,
 })
