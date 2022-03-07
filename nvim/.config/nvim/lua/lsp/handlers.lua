d40b0a1009a32891250c9a386e17b123bf6666d8
Matt Wright <wright.matthew91@gmail.com>
2022-03-07 12:50:09 -0700

    feat: add ts-utils from alvarez, disables 80001. lint

--- nvim/.config/nvim/lua/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lsp/handlers.lua
@@ -10,7 +10,10 @@ M.setup = function()
   }
 
   for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    vim.fn.sign_define(
+      sign.name,
+      { texthl = sign.name, text = sign.text, numhl = "" }
+    )
   end
 
   local config = {
@@ -35,13 +38,17 @@ M.setup = function()
 
   vim.diagnostic.config(config)
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-  })
+  vim.lsp.handlers["textDocument/hover"] =
+    vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+    })
 
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
-  })
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help,
+    {
+      border = "rounded",
+    }
+  )
 end
 
 local function lsp_highlight_document(client)
@@ -62,16 +69,58 @@ end
 
 local function lsp_keymaps(bufnr)
   local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "gD",
+    "<cmd>lua vim.lsp.buf.declaration()<CR>",
+    opts
+  )
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "gd",
+    "<cmd>lua vim.lsp.buf.definition()<CR>",
+    opts
+  )
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "K",
+    "<cmd>lua vim.lsp.buf.hover()<CR>",
+    opts
+  )
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "gi",
+    "<cmd>lua vim.lsp.buf.implementation()<CR>",
+    opts
+  )
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "<C-k>",
+    "<cmd>lua vim.lsp.buf.signature_help()<CR>",
+    opts
+  )
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "gr",
+    "<cmd>lua vim.lsp.buf.references()<CR>",
+    opts
+  )
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "[d",
+    '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>',
+    opts
+  )
   vim.api.nvim_buf_set_keymap(
     bufnr,
     "n",
@@ -79,14 +128,37 @@ local function lsp_keymaps(bufnr)
     '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
     opts
   )
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "]d",
+    '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>',
+    opts
+  )
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "<leader>q",
+    "<cmd>lua vim.diagnostic.setloclist()<CR>",
+    opts
+  )
+  vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
     client.resolved_capabilities.document_formatting = false
+    -- client.filter_out_diagnostics_by_code = {80001}
+    -- TODO: make this a pcall? clean a bit? does this have cool other things I should use?
+    local ts_utils = require("nvim-lsp-ts-utils")
+    ts_utils.setup({
+      filter_out_diagnostics_by_code = { 80001 },
+    })
+    ts_utils.setup_client(client)
+    -- require('nvim-lsp-ts-utils').setup({
+    --     filter_out_diagnostics_by_code = { 80001 },
+    -- })
+    -- require('nvim-lsp-ts-utils').setup_client(client)
   end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
