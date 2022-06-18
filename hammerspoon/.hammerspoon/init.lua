0c3d1a529468cd259f7c5d2d8abe8aea8a86288c
Matt Wright <wright.matthew91@gmail.com>
2022-06-17 23:52:15 -0600

    feat: checkpoint, up to spoons

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,17 +1,28 @@
--- hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
---   hs.alert.show("Hello World!")
--- end)
+-- TODO: Below might make things clean
+-- hs.hotkey.alertDuration = 0
+-- hs.hints.showTitleThresh = 0
+-- hs.window.animationDuration = 0
 
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
-  hs.notify.new({ title = "Hammerspoon", informativeText = "Hello World" }):send()
-end)
+-- printer
+-- TODO: move this and other utils to its own spoon
+local function bubble(str)
+  hs.alert.show(str)
+end
 
-hs.hotkey.bind({ "cmd", "shift", "ctrl" }, "R", function()
+-- load config
+local conf = hs.fs.pathToAbsolute(hs.configdir .. "/config.lua")
+if conf then
+  require("config")
+  print('*** config has been loaded ***')
+end
+
+-- reload hammerspoon config
+hs.hotkey.bind({ "cmd", "shift", "ctrl" }, "r", function()
   hs.reload()
 end)
 
-function reloadConfig(files)
-  doReload = false
+local function reloadConfig(files)
+  local doReload = false
   for _, file in pairs(files) do
     if file:sub(-4) == ".lua" then
       doReload = true
@@ -21,12 +32,50 @@ function reloadConfig(files)
     hs.reload()
   end
 end
-myWatcher = hs.pathwatcher.new(
-  os.getenv("HOME") .. "/.hammerspoon/",
-  reloadConfig
-):start()
 
+-- reload config when editing files in ~/.hammerspoon/*
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+-- display when config has been reloaded successfully
 hs.alert.show("Config loaded")
+
+
+-- hs.alert.sho
+-- hs.
+
+local foo = hs.fs.displayName(hs.configdir .. '/Spoons/ModalMgr')
+if not foo then
+  hs.alert.show('oh no, not foo')
+  return
+end
+
+hs.alert.show('foo: ' .. foo)
+
+
+local function checkForSpoon(name)
+  
+end
+-- local function reloadConfig(files)
+--   local doReload = false
+--   for _, file in pairs(files) do
+--     if file:sub(-4) == ".lua" then
+--       doReload = true
+--     end
+--   end
+--   if doReload then
+--     hs.reload()
+--   end
+-- end
+
+-- hs.fs.link(hs.configdir .. '')
+
+-- ModalMgr Spoon must be loaded explicitly, because this repository heavily relies upon it.
+-- hs.loadSpoon("ModalMgr")
+
+-- notes below, probably trash
+
+-- below will not show when Do Not Disturb is on
+-- hs.notify.new({ title = "Hammerspoon", informativeText = "Hello World" }):send()
 
 -- MOVING A WINDOW TO A NEW SPACE -- START
 -- local spaces = require("hs._asm.undocumented.spaces")
