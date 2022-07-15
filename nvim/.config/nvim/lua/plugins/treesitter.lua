270c3b8addc5d5ddc3b33fb28152b455cfd4d425
Matt Wright <wright.matthew91@gmail.com>
2022-07-15 18:25:35 -0400

    feat: ensure_installed now a list for no errors on m1

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,89 +1,116 @@
 local M = {}
 function M:configure()
-	-- print("configuring treesitter")
-	local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-	if not status_ok then
-		print("error: cannot configure treesitter inside configure function")
-		return
-	end
+  -- print("configuring treesitter")
+  local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+  if not status_ok then
+    print("error: cannot configure treesitter inside configure function")
+    return
+  end
 
-	configs.setup({
-		ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-		sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-		ignore_install = { "" }, -- List of parsers to ignore installing
-		highlight = {
-			enable = true, -- false will disable the whole extension
-			disable = {}, -- list of language that will be disabled, e.g. {"latex", "c", "rust"}
-			additional_vim_regex_highlighting = true,
-		},
-		-- TODO: keep indent experimental feature? could cause some issues
-		indent = {
-			enable = true,
-			disable = { "python", "yaml" }, -- {"python", "html", "javascript"},
-		},
-		-- other plugin modules
-		context_commentstring = {
-			enable = true,
-			enable_autocmd = false,
-			-- config = {
-			--   -- Languages that have a single comment style
-			--   typescript = "// %s",
-			--   css = "/* %s */",
-			--   scss = "/* %s */",
-			--   html = "<!-- %s -->",
-			--   svelte = "<!-- %s -->",
-			--   vue = "<!-- %s -->",
-			--   json = "",
-			-- },
-		},
-		autopairs = {
-			enable = true,
-		},
-		-- TODO: use some of these?
-		-- matchup = {
-		--   enable = false, -- mandatory, false will disable the whole extension
-		--   -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
-		-- },
-		-- autotag = { enable = false },
-		-- textobjects = {
-		--   swap = {
-		--     enable = false,
-		--     -- swap_next = textobj_swap_keymaps,
-		--   },
-		--   -- move = textobj_move_keymaps,
-		--   select = {
-		--     enable = false,
-		--     -- keymaps = textobj_sel_keymaps,
-		--   },
-		-- },
-		-- textsubjects = {
-		--   enable = false,
-		--   keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
-		-- },
-		-- playground = {
-		--   enable = false,
-		--   disable = {},
-		--   updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-		--   persist_queries = false, -- Whether the query persists across vim sessions
-		--   keybindings = {
-		--     toggle_query_editor = "o",
-		--     toggle_hl_groups = "i",
-		--     toggle_injected_languages = "t",
-		--     toggle_anonymous_nodes = "a",
-		--     toggle_language_display = "I",
-		--     focus_language = "f",
-		--     unfocus_language = "F",
-		--     update = "R",
-		--     goto_node = "<cr>",
-		--     show_help = "?",
-		--   },
-		-- },
-		-- rainbow = {
-		--   enable = false,
-		--   extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-		--   max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-		-- },
-	})
+  configs.setup({
+    ensure_installed = {
+      "python",
+      "javascript",
+      "json",
+      "yaml",
+      "lua",
+      "fish",
+      "go",
+      "rust",
+      "css",
+      "cmake",
+      "hjson",
+      "swift",
+      "c",
+      "typescript",
+      "make",
+      "vim",
+      "dockerfile",
+      "markdown_inline",
+      "vue",
+      "markdown",
+      "sql",
+      "html",
+      "cpp",
+      "c_sharp",
+      "latex",
+    }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    -- ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+    ignore_install = { "" }, -- List of parsers to ignore installing
+    highlight = {
+      enable = true, -- false will disable the whole extension
+      disable = {}, -- list of language that will be disabled, e.g. {"latex", "c", "rust"}
+      additional_vim_regex_highlighting = true,
+    },
+    -- TODO: keep indent experimental feature? could cause some issues
+    indent = {
+      enable = true,
+      disable = { "python", "yaml" }, -- {"python", "html", "javascript"},
+    },
+    -- other plugin modules
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+      -- config = {
+      --   -- Languages that have a single comment style
+      --   typescript = "// %s",
+      --   css = "/* %s */",
+      --   scss = "/* %s */",
+      --   html = "<!-- %s -->",
+      --   svelte = "<!-- %s -->",
+      --   vue = "<!-- %s -->",
+      --   json = "",
+      -- },
+    },
+    autopairs = {
+      enable = true,
+    },
+    -- TODO: use some of these?
+    -- matchup = {
+    --   enable = false, -- mandatory, false will disable the whole extension
+    --   -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+    -- },
+    -- autotag = { enable = false },
+    -- textobjects = {
+    --   swap = {
+    --     enable = false,
+    --     -- swap_next = textobj_swap_keymaps,
+    --   },
+    --   -- move = textobj_move_keymaps,
+    --   select = {
+    --     enable = false,
+    --     -- keymaps = textobj_sel_keymaps,
+    --   },
+    -- },
+    -- textsubjects = {
+    --   enable = false,
+    --   keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
+    -- },
+    -- playground = {
+    --   enable = false,
+    --   disable = {},
+    --   updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    --   persist_queries = false, -- Whether the query persists across vim sessions
+    --   keybindings = {
+    --     toggle_query_editor = "o",
+    --     toggle_hl_groups = "i",
+    --     toggle_injected_languages = "t",
+    --     toggle_anonymous_nodes = "a",
+    --     toggle_language_display = "I",
+    --     focus_language = "f",
+    --     unfocus_language = "F",
+    --     update = "R",
+    --     goto_node = "<cr>",
+    --     show_help = "?",
+    --   },
+    -- },
+    -- rainbow = {
+    --   enable = false,
+    --   extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+    --   max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+    -- },
+  })
 end
 
 M.configure()
