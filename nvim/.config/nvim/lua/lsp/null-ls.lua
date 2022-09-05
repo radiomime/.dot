5dd2d596c125d9ac1394c28bf1e43b39fcff2906
Matt Wright <wright.matthew91@gmail.com>
2022-09-05 15:37:16 -0600

    feat: add shellharden and shfmt

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/lsp/null-ls.lua
@@ -15,18 +15,21 @@ null_ls.setup({
     formatting.prettier, -- with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
     formatting.black.with({ extra_args = { "--fast" } }),
     formatting.stylua, -- using sumneko, though that may have memory leaks
+    --[[ formatting.beautysh, ]]
+    formatting.shfmt,
+    formatting.shellharden,
     -- diagnostics.flake8
   },
 
   -- uncomment on_attach fn below for format on save
   on_attach = function(client)
-  	if client.resolved_capabilities.document_formatting then
-  		vim.cmd([[
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd([[
             augroup LspFormatting
                 autocmd! * <buffer>
                 autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
             augroup END
             ]])
-  	end
+    end
   end,
 })
