07c40afc97cb8dab7e044ba2652372798f8f93ce
Matt Wright <wright.matthew91@gmail.com>
2022-09-09 12:36:10 -0600

    feat: quickscope colors and underline

--- nvim/.config/nvim/lua/plugins/_quickscope.lua
+++ nvim/.config/nvim/lua/plugins/_quickscope.lua
@@ -9,6 +9,14 @@ function M:configure()
   -- Unless you are still migrating, remove the deprecated commands from v1.x
   vim.cmd([[ let g:qs_highlight_on_keys = ['f', 'F', 't', 'T'] ]])
   --[[ let g:qs_highlight_on_keys = ['f', 'F', 't', 'T'] ]]
+
+  -- TODO: this could be nice to make the color different
+  vim.cmd(
+    [[ highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline ]]
+  )
+  vim.cmd(
+    [[ highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline ]]
+  )
 end
 
 M.configure()
