8c9d9c0ff48526233eb455262549d9f015444d29
Matt Wright <wright.matthew91@gmail.com>
2023-01-04 17:19:28 -0700

    docs: todo

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -294,6 +294,7 @@ function M:_install()
       },
     })
 
+    -- TODO: add some new keymaps! https://github.com/johmsalas/text-case.nvim#setup
     use("johmsalas/text-case.nvim")
     -----
     -- TODO: below are some of the old ones I've used
