d28bba2883cb99272a30b3da7f46532f5c347cc0
Matt Wright <wright.matthew91@gmail.com>
2023-06-23 22:09:36 -0600

    feat: gcloud

--- fish/.config/fish/workbook.local.config.fish
+++ fish/.config/fish/workbook.local.config.fish
@@ -6,3 +6,8 @@ if test -f /usr/local/Caskroom/miniconda/base/bin/conda
     eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
 end
 # <<< conda initialize <<<
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/zpuser/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/zpuser/google-cloud-sdk/path.fish.inc'
+end
