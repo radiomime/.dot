85226bb4d9bd88751e12f02a7d07f3bdcc1313bd
Matt Wright <wright.matthew91@gmail.com>
2023-09-18 13:35:29 -0600

    docs: some startup comments

--- fish/.config/fish/workbook.local.config.fish
+++ fish/.config/fish/workbook.local.config.fish
@@ -1,5 +1,6 @@
 alias pulumi_cred='GOOGLE_APPLICATION_CREDENTIALS=$(pulumi config get gcp:credentials) pulumi'
 
+echo about to init conda
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /usr/local/Caskroom/miniconda/base/bin/conda
@@ -7,6 +8,8 @@ if test -f /usr/local/Caskroom/miniconda/base/bin/conda
 end
 # <<< conda initialize <<<
 
+# google cloud sdk through brew
+echo activate base for google cloud sdk
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/zpuser/google-cloud-sdk/path.fish.inc' ]
     . '/Users/zpuser/google-cloud-sdk/path.fish.inc'
