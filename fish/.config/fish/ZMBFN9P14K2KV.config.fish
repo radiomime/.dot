f4c6db83d1eadf7607e0c6cabd0a24690ab49c3c
Matt Wright <wright.matthew91@gmail.com>
2023-10-03 10:40:40 -0400

    feat: workbook config

---
+++ fish/.config/fish/ZMBFN9P14K2KV.config.fish
@@ -0,0 +1,12 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/mattwright/miniconda3/bin/conda
+    eval /Users/mattwright/miniconda3/bin/conda "shell.fish" hook $argv | source
+end
+# <<< conda initialize <<<
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mattwright/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/mattwright/google-cloud-sdk/path.fish.inc'
+end
