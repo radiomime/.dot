ee99f9dcf83bed7db56f174c69c82590ff842559
Matt Wright <wright.matthew91@gmail.com>
2024-04-13 23:41:30 -0600

    feat: vv spacing, include default config

--- fish/.config/fish/functions/vv.fish
+++ fish/.config/fish/functions/vv.fish
@@ -1,6 +1,20 @@
 function vv
     # Assumes all configs exist in directories named ~/.config/nvim-*
-    set config (fd --max-depth 1 --glob 'nvim-*' ~/.config | fzf --prompt="Neovim Configs > " --height=~50% --layout=reverse --border --exit-0)
+    # set config (fd --max-depth 1 --glob 'nvim-*' ~/.config | fzf --prompt="Neovim Configs > " --height=~50% --layout=reverse --border --exit-0)
+    set config (
+        fd \
+        --max-depth 1 \
+        # --glob '*' ~/.config \
+        --glob 'nvim*' ~/.config \
+        # --glob 'nvim-*' ~/.config \
+        | \
+        fzf \
+        --prompt="Neovim Configs > " \
+        --height=~50% \
+        --layout=reverse \
+        --border \
+        --exit-0\
+    )
 
     # If I exit fzf without selecting a config, don't open Neovim
     if test -z $config
@@ -10,5 +24,6 @@ function vv
 
     # Open Neovim with the selected config
     set -x NVIM_APPNAME (basename $config)
+
     nvim $argv
 end
