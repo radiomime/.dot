2d6d53724f64ce67249add7de6576de8d0126fca
Matt Wright <wright.matthew91@gmail.com>
2024-04-13 23:27:31 -0600

    feat: vv fn

---
+++ fish/.config/fish/functions/vv.fish
@@ -0,0 +1,14 @@
+function vv
+    # Assumes all configs exist in directories named ~/.config/nvim-*
+    set config (fd --max-depth 1 --glob 'nvim-*' ~/.config | fzf --prompt="Neovim Configs > " --height=~50% --layout=reverse --border --exit-0)
+
+    # If I exit fzf without selecting a config, don't open Neovim
+    if test -z $config
+        echo "No config selected"
+        return
+    end
+
+    # Open Neovim with the selected config
+    set -x NVIM_APPNAME (basename $config)
+    nvim $argv
+end
