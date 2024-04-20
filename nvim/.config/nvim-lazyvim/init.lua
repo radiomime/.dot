8ff678bd46f8a1b61d67729754dbc5ee25d4564c
Matt Wright <wright.matthew91@gmail.com>
2024-04-20 14:05:06 -0600

    init updates

--- nvim/.config/nvim-lazyvim/init.lua
+++ nvim/.config/nvim-lazyvim/init.lua
@@ -1,9 +1,11 @@
+require("kickstart")
+
 require("options")
 require("keymaps")
 require("plugins")
--- require("plugins").install({})
-require("colorscheme")
--- require("plugins.cmp") -- TODO: do I need to configure cmp before lsp?
-require("lsp")
--- require("plugins").configure({})
-require("autocommands")
+-- -- require("plugins").install({})
+-- require("colorscheme")
+-- -- require("plugins.cmp") -- TODO: do I need to configure cmp before lsp?
+-- require("lsp")
+-- -- require("plugins").configure({})
+-- require("autocommands")
