f1a38174c5fbf9337782825533ae2519748e35d3
Matt Wright <wright.matthew91@gmail.com>
2024-04-20 22:11:44 -0600

    keymaps start

--- nvim/dot-config/nvim/lua/keymaps.lua
+++ nvim/dot-config/nvim/lua/keymaps.lua
@@ -1,5 +1,112 @@
 print("setting keymappings...")
 
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+-- vim.g.have_nerd_font = false
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Diagnostic keymaps
+vim.keymap.set(
+  "n",
+  "[d",
+  vim.diagnostic.goto_prev,
+  { desc = "Go to previous [D]iagnostic message" }
+)
+vim.keymap.set(
+  "n",
+  "]d",
+  vim.diagnostic.goto_next,
+  { desc = "Go to next [D]iagnostic message" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>e",
+  vim.diagnostic.open_float,
+  { desc = "Show diagnostic [E]rror messages" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>q",
+  vim.diagnostic.setloclist,
+  { desc = "Open diagnostic [Q]uickfix list" }
+)
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set(
+  "t",
+  "<Esc><Esc>",
+  "<C-\\><C-n>",
+  { desc = "Exit terminal mode" }
+)
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set(
+  "n",
+  "<C-h>",
+  "<C-w><C-h>",
+  { desc = "Move focus to the left window" }
+)
+vim.keymap.set(
+  "n",
+  "<C-l>",
+  "<C-w><C-l>",
+  { desc = "Move focus to the right window" }
+)
+vim.keymap.set(
+  "n",
+  "<C-j>",
+  "<C-w><C-j>",
+  { desc = "Move focus to the lower window" }
+)
+vim.keymap.set(
+  "n",
+  "<C-k>",
+  "<C-w><C-k>",
+  { desc = "Move focus to the upper window" }
+)
+
+-- old keymaps...
+
+------------------------
+----- insert mode ------
+------------------------
+-- insert -> normal mode heaven
+vim.keymap.set("i", "jk", "<esc>", { silent = true })
+vim.keymap.set("i", "kj", "<ESC>", { silent = true })
+
+-- Move current line / block with ctrl-j/k ala vscode.
+-- vim.keymap.set("i", "<C-j>", "<Esc>:m .+1<CR>==gi", { silent = true })
+-- vim.keymap.set("i", "<C-k>", "<Esc>:m .-2<CR>==gi", { silent = true })
+
+-- navigate tab completion with <c-j> and <c-k>
+-- runs conditionally
+-- vim.keymap.set(
+--   "i",
+--   "<C-j>",
+--   'pumvisible() ? "\\<C-n>" : "\\<C-j>"',
+--   { expr = true, noremap = true }
+-- )
+--
+-- vim.keymap.set(
+--   "i",
+--   "<C-k>",
+--   'pumvisible() ? "\\<C-p>" : "\\<C-k>"',
+--   { expr = true, noremap = true }
+-- )
+
 local M = {}
 
 function M.set_keymappings()
@@ -12,34 +119,36 @@ function M.set_keymappings()
 
   --Remap space as leader key
   keymap("", "<Space>", "<Nop>", opts)
-  vim.g.mapleader = " "
-  vim.g.maplocalleader = ","
+  -- --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+  -- vim.g.mapleader = " "
+  -- vim.g.maplocalleader = ","
 
   ------------------------
   ----- insert mode ------
   ------------------------
   -- insert -> normal mode heaven
-  keymap("i", "jk", "<ESC>", opts)
-  keymap("i", "kj", "<ESC>", opts)
+  -- keymap("i", "jk", "<ESC>", opts)
+  -- keymap("i", "kj", "<ESC>", opts)
 
-  -- Move current line / block with ctrl-j/k ala vscode.
-  keymap("i", "<C-j>", "<Esc>:m .+1<CR>==gi", opts)
-  keymap("i", "<C-k>", "<Esc>:m .-2<CR>==gi", opts)
+  -- -- Move current line / block with ctrl-j/k ala vscode.
+  -- keymap("i", "<C-j>", "<Esc>:m .+1<CR>==gi", opts)
+  -- keymap("i", "<C-k>", "<Esc>:m .-2<CR>==gi", opts)
 
   -- navigate tab completion with <c-j> and <c-k>
   -- runs conditionally
-  keymap(
-    "i",
-    "<C-j>",
-    'pumvisible() ? "\\<C-n>" : "\\<C-j>"',
-    { expr = true, noremap = true }
-  )
-  keymap(
-    "i",
-    "<C-k>",
-    'pumvisible() ? "\\<C-p>" : "\\<C-k>"',
-    { expr = true, noremap = true }
-  )
+  --  keymap(
+  --    "i",
+  --    "<C-j>",
+  --    'pumvisible() ? "\\<C-n>" : "\\<C-j>"',
+  --    { expr = true, noremap = true }
+  --  )
+  --
+  --  keymap(
+  --    "i",
+  --    "<C-k>",
+  --    'pumvisible() ? "\\<C-p>" : "\\<C-k>"',
+  --    { expr = true, noremap = true }
+  --  )
 
   ------------------------
   ----- normal mode ------
