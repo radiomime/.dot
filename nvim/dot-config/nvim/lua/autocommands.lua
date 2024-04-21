666318f46c13043082ac33df6626d0e6ee3e472d
Matt Wright <wright.matthew91@gmail.com>
2024-04-20 22:11:35 -0600

    added basic

--- nvim/dot-config/nvim/lua/autocommands.lua
+++ nvim/dot-config/nvim/lua/autocommands.lua
@@ -1,9 +1,26 @@
 local M = {}
 
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup(
+    "kickstart-highlight-yank",
+    { clear = true }
+  ),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- TODO: play around with these
 function M:create_autocommands()
-	-- print("creating autocommands")
-	vim.cmd([[
+  -- print("creating autocommands")
+  vim.cmd([[
   augroup _general_settings
     autocmd!
     autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
@@ -36,11 +53,11 @@ function M:create_autocommands()
 ]])
 
   -- TODO: format on save?
-	-- Autoformat
-	-- augroup _lsp
-	--   autocmd!
-	--   autocmd BufWritePre * lua vim.lsp.buf.formatting()
-	-- augroup end
+  -- Autoformat
+  -- augroup _lsp
+  --   autocmd!
+  --   autocmd BufWritePre * lua vim.lsp.buf.formatting()
+  -- augroup end
 end
 
 M.create_autocommands()
