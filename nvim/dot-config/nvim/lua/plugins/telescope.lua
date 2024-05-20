d8dc265b5bb3a05bbf537e3a755ca277680820ee
Matt Wright <wright.matthew91@gmail.com>
2024-05-20 12:35:01 -0600

    telescope updates to keys

--- nvim/dot-config/nvim/lua/plugins/telescope.lua
+++ nvim/dot-config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     "nvim-telescope/telescope.nvim",
     keys = {
@@ -10,15 +9,17 @@ return {
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
       },
+      { "<leader>ff", LazyVim.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+      { "<leader>fF", LazyVim.telescope("files"), desc = "Find Files (Root Dir)" },
     },
     -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
+    -- opts = {
+    --   defaults = {
+    --     layout_strategy = "horizontal",
+    --     layout_config = { prompt_position = "top" },
+    --     sorting_strategy = "ascending",
+    --     winblend = 0,
+    --   },
+    -- },
   },
 }
