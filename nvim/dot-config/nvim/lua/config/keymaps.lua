9de95606ef6e21904f29fc9ad7af3d6fe8010e41
Matt Wright <wright.matthew91@gmail.com>
2024-05-20 12:35:20 -0600

    terminal mappings and removed some keys

--- nvim/dot-config/nvim/lua/config/keymaps.lua
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- NOTE: Remember that Lua is a real programming language, and as such it is possible
+-- to define small helper and utility functions so you don't have to repeat yourself.
+--
+-- local del = function(mode, lhs)
+--   vim.keymap.del(mode, lhs)
+-- end
+-- del("n", "<leader>ww")
+vim.keymap.del("n", "<leader>ww")
+vim.keymap.del("n", "<leader>wd")
+vim.keymap.del("n", "<leader>w-")
+vim.keymap.del("n", "<leader>w|")
 
 ------------------------
 ----- insert mode ------
@@ -20,5 +32,14 @@ vim.keymap.set("i", "kj", "<ESC>", { silent = true })
 -- local keymap = vim.api.nvim_set_keymap
 
 -- ; to : for ease of use, but keep ; as an option
+local opts = { noremap = true, silent = true }
 vim.keymap.set("n", ";", ":", opts)
 vim.keymap.set("n", ":", ";", opts)
+
+------------------------
+----- terminal mode ------
+------------------------
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+vim.keymap.set("t", "jk", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
