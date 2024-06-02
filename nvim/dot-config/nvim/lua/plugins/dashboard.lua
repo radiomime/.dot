eedb47faafd5f91be44ef92d5fc30e93ee6a12ab
Matt Wright <wright.matthew91@gmail.com>
2024-06-02 19:32:51 +0000

    dashboard update

---
+++ nvim/dot-config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,17 @@
+local logo = [[
+      lol what...
+      is this...
+]]
+logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    opts = {
+
+      config = {
+        -- header = vim.split(logo, "\n"),
+      },
+    },
+  },
+}
