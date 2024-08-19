373f6e60ab345774b840bfed20fe9b239632c89f
Matt Wright <mattwright@zmbfn9p14k2kv.lan>
2024-08-19 15:23:47 -0600

    fish config update

--- fish/dot-config/fish/ZMBFN9P14K2KV.config.fish
+++ fish/dot-config/fish/ZMBFN9P14K2KV.config.fish
@@ -10,3 +10,16 @@ end
 if [ -f '/Users/mattwright/google-cloud-sdk/path.fish.inc' ]
     . '/Users/mattwright/google-cloud-sdk/path.fish.inc'
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/mattwright/miniconda3/bin/conda
+    eval /Users/mattwright/miniconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/Users/mattwright/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/Users/mattwright/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /Users/mattwright/miniconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<<
