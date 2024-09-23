a033da7c7d95008f566b942afd20d59b4cdf92c6
Matt Wright <wright.matthew91@gmail.com>
2024-09-23 10:01:51 -0600

    supermaven

---
+++ nvim/dot-config/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
+}
