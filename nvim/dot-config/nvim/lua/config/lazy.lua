c73344481178541a23dd495c651d9ef30718afb8
Matt Wright <wright.matthew91@gmail.com>
2024-10-23 11:36:18 -0600

    supermaven, no copilot

--- nvim/dot-config/nvim/lua/config/lazy.lua
+++ nvim/dot-config/nvim/lua/config/lazy.lua
@@ -14,8 +14,9 @@ require("lazy").setup({
 
     -- import any extras modules here --
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
