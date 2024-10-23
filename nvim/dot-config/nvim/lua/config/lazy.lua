0ff03cf4989f1affe04686aa4e310f91f9b8cf14
Matt Wright <wright.matthew91@gmail.com>
2024-10-22 23:03:34 -0600

    updates

--- nvim/dot-config/nvim/lua/config/lazy.lua
+++ nvim/dot-config/nvim/lua/config/lazy.lua
@@ -14,8 +14,8 @@ require("lazy").setup({
 
     -- import any extras modules here --
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
